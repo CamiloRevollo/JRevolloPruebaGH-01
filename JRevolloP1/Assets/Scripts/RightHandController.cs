a7e2f469b1b10e33545ef5b6c98e82e24d7fb721
CamiloRevollo <camimorevollo1302@gmail.com>
2025-05-20 20:14:17 -0500

    Ultima configuracion
    escena base VR; proyecto personal
    
    XR ORIGIN
    Tunelling Vignette
    Teleportation
    CTURN Y  STURN CMOVE

--- JRevolloP1/Assets/Scripts/RightHandController.cs
+++ JRevolloP1/Assets/Scripts/RightHandController.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class RightHandController : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private XRRayInteractor xRRayInteractor_Grap;
+    [SerializeField] private XRRayInteractor xRRayInteractor_Teleport;
+
+    [SerializeField] private InputActionReference _Joystick_North_Sector;
+
+    private void Awake()
     {
-        
+        xRRayInteractor_Teleport.enabled = false;
+
+    }
+    private void OnEnable()
+    {
+        _Joystick_North_Sector.action.performed += PalanArribaPresionada;
+        _Joystick_North_Sector.action.canceled += PalanArribaPresionada;
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    private void OnDisable()
     {
-        
+        _Joystick_North_Sector.action.performed -= PalanArribaPresionada;
+        _Joystick_North_Sector.action.canceled -= PalanArribaLiberada;
     }
+    private void PalanArribaPresionada(InputAction.CallbackContext context)
+    {
+        xRRayInteractor_Grap.enabled = false;
+        xRRayInteractor_Teleport.enabled = true;
+
+
+    }
+
+    private void PalanArribaLiberada(InputAction.CallbackContext context) => Invoke("PalanArribaLiberada_Invoke", 0.01f);
+    private void PalanArribaLiberada_Invoke()
+    {
+        xRRayInteractor_Grap.enabled = true;
+        xRRayInteractor_Teleport.enabled = false;
+
+
+    }
+
 }
